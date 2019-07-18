fb588091004466ff3d40ce1101c8e955d2fb1494
PhD20 <dmprofessor227@gmail.com>
2019-07-17 20:18:01 -0500

    Add build

---
+++ rpghub/build/precache-manifest.4885b0198f3a06d7179f1967b96947ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "954f8076cb802c66255a83280e7ecb13",
+    "url": "/rpghub/index.html"
+  },
+  {
+    "revision": "cfe6af44707ec0bafa48",
+    "url": "/rpghub/static/css/2.fb390452.chunk.css"
+  },
+  {
+    "revision": "84a39f9480ed2d9ab1b7",
+    "url": "/rpghub/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "cfe6af44707ec0bafa48",
+    "url": "/rpghub/static/js/2.3cf1cc9f.chunk.js"
+  },
+  {
+    "revision": "84a39f9480ed2d9ab1b7",
+    "url": "/rpghub/static/js/main.1a6a5539.chunk.js"
+  },
+  {
+    "revision": "e760f29961057a0f3ec8",
+    "url": "/rpghub/static/js/runtime~main.fefc12d8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rpghub/static/media/logo.5d5d9eef.svg"
+  }
+]);
